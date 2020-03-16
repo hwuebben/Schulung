d4aee51e846ba3fd57f33ae0502f4df6152064aa
Sven Ogorek <s.ogorek@web.de>
2020-03-16 15:03:24 +0100

    alles auf Anfang

--- ini_test.py
+++ ini_test.py
@@ -4,11 +4,3 @@ size = 10
 
 board = erzeuge_individuum(size)
 assert len(board.flat) == size**2
-
-assert berechne_fitness(board) > 0
-
-value = berechne_fitness(board)
-
-print(type(value))
-
-assert type(value) is np.float64
