85fce0a226aadd970821f672dcc02a1abfebcd19
Sven Ogorek <s.ogorek@web.de>
2020-03-16 14:54:39 +0100

    fitness test typagfrage gefixt

--- ini_test.py
+++ ini_test.py
@@ -5,4 +5,9 @@ board = erzeuge_individuum(size)
 assert len(board.flat) == size**2
 
 assert berechne_fitness(board) > 0
-assert type(berechne_fitness(board)) == 'numpy.float64'
+
+value = berechne_fitness(board)
+
+print(type(value))
+
+assert type(value) == "<class 'numpy.float64'>"
