92dd171a52652698376708d05892efe83cb8fe32
Simon Klein <simon.klein@outlook.de>
2020-03-16 14:39:38 +0100

    second test

--- tests/EA_test.py
+++ tests/EA_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from EA import erzeuge_individuum
+from EA import *
 
 def test_erzeuge_individuum():
     for n in range(10):
@@ -10,4 +10,8 @@ def test_erzeuge_individuum():
         assert np.count_nonzero(x[:]) == n
 
 def test_berechne_fitness():
-    pass
+    for n in range(1,10):
+        xn = berechne_fitness(erzeuge_individuum(n))
+        assert xn >= 0. 
+        assert xn <= 1.
+        assert xn.dtype == np.float
