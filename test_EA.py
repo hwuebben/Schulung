15e68fe05333e199332886ca0f33795cbc96775b
Sven Ogorek <s.ogorek@web.de>
2020-03-16 15:43:54 +0100

    Schauen ob es klappt

--- test_EA.py
+++ test_EA.py
@@ -3,6 +3,9 @@ from EA import *
 def choice_func(x, size, replace):
     return x[0:size]
 
-for n in range(1,10):
-    board = erzeuge_individuum(n, choice_func)
-    assert len(board.flat) == n**2
+def test_inid():
+    for n in range(1,10):
+        board = erzeuge_individuum(n, choice_func)
+        assert len(board.flat) == n**2
+
+test_inid()
